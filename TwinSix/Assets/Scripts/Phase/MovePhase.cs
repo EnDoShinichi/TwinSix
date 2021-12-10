cddbac1813cb556fecfd2dba914d75422092d60c
GTA20_Kaede <nkc-kaede>
2021-12-10 16:43:39 +0900

    add:少し進んだ

--- TwinSix/Assets/Scripts/Phase/MovePhase.cs
+++ TwinSix/Assets/Scripts/Phase/MovePhase.cs
@@ -6,7 +6,31 @@ using UnityEngine;
 public class MovePhase : MonoBehaviour,IPhase
 {
     public event Action NextPhase;
-    private PlayerStatus playerStatus;
+    public PlayerStatus playerStatus;
+
+    private bool doubtFlg;
+
+
+
+    private void Start()
+    {
+        //システムのランダムとUnityが競合した
+        int random = UnityEngine.Random.Range(1, 6);
+        int random1 = UnityEngine.Random.Range(1, 6);
+        playerStatus.SetDice(random);
+        playerStatus.SetDoubtDice(random1);
+        playerStatus.AddDoubtCount(5);
+        playerStatus.SetDoubt(false);
+
+        Debug.Log(playerStatus.dice);
+        Debug.Log(playerStatus.doubtDice);
+        Debug.Log(playerStatus.doubt);
+    }
+
+    private void Update()
+    {
+        
+    }
 
     public void PhaseEnd()
     {
@@ -15,14 +39,46 @@ public class MovePhase : MonoBehaviour,IPhase
 
     public void PhaseStart(PlayerStatus turnObject)
     {
-
+        playerStatus = turnObject;
     }
 
     public void PhaseUpdate()
     {
-        //if(playerStatus.SetDoubtDice())
+        if(playerStatus.doubt)
         {
+            PlayerSetMapDoubt();
+        }
+        else
+        {
+            PlayerSetMapTruth();
+        }
+        NextPhase();
+    }
 
+    /// <summary>
+    /// サイコロの目が嘘
+    /// </summary>
+    void PlayerSetMapDoubt()
+    {
+        // 嘘の数値分、進む
+        for (int i = 0; i < playerStatus.doubtDice; i++)
+        {
+            playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
+        }
+        playerStatus.AddDoubtCount(1);
+    }
+
+    /// <summary>
+    /// サイコロの目が正しい
+    /// </summary>
+    void PlayerSetMapTruth()
+    {
+        // サイコロの出た目通りに進む
+        for (int i = 0; i <= playerStatus.dice; i++)
+        {
+            // Vector3 nextPos = playerStatus.myMapPosition.nextMapData[0];
+            // 今の場所からnextPosに進む処理
+            playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
         }
     }
 }
