86b242f5c8a48820fe40b61bfebc45d96e5add46
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2022-01-14 14:54:53 +0900

    add.DiceFhase完

---
+++ TwinSix/Assets/Scripts/TakaoScripts/SquaresWindow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SquaresWindow : EditorWindow
+{
+    [MenuItem("Window/Squares Window")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(SquaresWindow));
+    }
+    private void OnGUI() //実際のウィンドウのコード
+    {
+        
+    }
+}
