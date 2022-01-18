0efc8a3f5b023b918325c9d98ca8c32e9b6b984b
GTA20_Kaede <nkc-kaede>
2022-01-18 16:04:21 +0900

    add:内部動作はだいたい完成

--- TwinSix/Assets/Scripts/Phase/MovePhase.cs
+++ TwinSix/Assets/Scripts/Phase/MovePhase.cs
@@ -8,29 +8,29 @@ public class MovePhase : MonoBehaviour,IPhase
     public event Action NextPhase;
     public PlayerStatus playerStatus;
 
-    private bool doubtFlg;
+    [SerializeField] private float waitTime;
+    [SerializeField] private MapInfoScriptableObject playerMap;
 
+    private bool choiceFlg;
 
-
-    private void Start()
+    /*private void Start()
     {
         //システムのランダムとUnityが競合した
         int random = UnityEngine.Random.Range(1, 6);
         int random1 = UnityEngine.Random.Range(1, 6);
-        playerStatus.SetDice(random);
-        playerStatus.SetDoubtDice(random1);
-        playerStatus.AddDoubtCount(5);
+        playerStatus.SetDice(3);
+        playerStatus.SetDoubtDice(4);
         playerStatus.SetDoubt(false);
 
-        Debug.Log(playerStatus.dice);
-        Debug.Log(playerStatus.doubtDice);
-        Debug.Log(playerStatus.doubt);
-    }
+        playerStatus.SetMap(playerMap);
 
-    private void Update()
-    {
-        
-    }
+        Debug.Log($"真({playerStatus.dice})");
+        Debug.Log($"偽({playerStatus.doubtDice})");
+        //Debug.Log(playerStatus.doubt);
+        //Debug.Log(playerStatus.myMapPosition.nextMapData.Length);
+
+        PhaseStart(playerStatus);
+    }*/
 
     public void PhaseEnd()
     {
@@ -40,45 +40,80 @@ public class MovePhase : MonoBehaviour,IPhase
     public void PhaseStart(PlayerStatus turnObject)
     {
         playerStatus = turnObject;
+
+        if (playerStatus.doubt) StartCoroutine(PlayerSetMapDoubt());
+        else StartCoroutine(PlayerSetMapTruth());
+
+        
     }
 
     public void PhaseUpdate()
     {
-        if(playerStatus.doubt)
-        {
-            PlayerSetMapDoubt();
-        }
-        else
-        {
-            PlayerSetMapTruth();
-        }
-        NextPhase();
+        
     }
 
     /// <summary>
     /// サイコロの目が嘘
     /// </summary>
-    void PlayerSetMapDoubt()
+    IEnumerator PlayerSetMapDoubt()
     {
         // 嘘の数値分、進む
         for (int i = 0; i < playerStatus.doubtDice; i++)
         {
-            playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
+            //playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
+
+            ChoiceRoad();
+
+            if (choiceFlg == true) break;
+
+            if (playerStatus.myMapPosition.mapEventData.EventTypeGet() == EventType.STAND) break;
+
+            yield return new WaitForSeconds(waitTime);
+
+            //Debug.Log("asd");
         }
         playerStatus.AddDoubtCount(1);
+        choiceFlg = false;
+        NextPhase();
     }
 
     /// <summary>
     /// サイコロの目が正しい
     /// </summary>
-    void PlayerSetMapTruth()
+    IEnumerator PlayerSetMapTruth()
     {
         // サイコロの出た目通りに進む
-        for (int i = 0; i <= playerStatus.dice; i++)
+        for (int i = 0; i < playerStatus.dice; i++)
         {
             // Vector3 nextPos = playerStatus.myMapPosition.nextMapData[0];
             // 今の場所からnextPosに進む処理
-            playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
+            //playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
+
+            ChoiceRoad();
+
+            if (choiceFlg == true) break;
+            
+            if (playerStatus.myMapPosition.mapEventData.EventTypeGet() == EventType.STAND) break;
+
+            yield return new WaitForSeconds(waitTime);
+
+            //Debug.Log("fgh");
         }
+        choiceFlg = false;
+        NextPhase();
+    }
+
+    /// <summary>
+    /// 分かれ道があるかどうか
+    /// </summary>
+    void ChoiceRoad()
+    {
+        if (playerStatus.myMapPosition.nextMapData.Length >= 2)
+        {
+            choiceFlg = true;
+
+            // 進行方向を決める処理を追加しないと行けない
+        }
+        else playerStatus.SetMap(playerStatus.myMapPosition.nextMapData[0]);
     }
 }
